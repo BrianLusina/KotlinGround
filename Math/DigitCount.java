b8a5874cbf59d52de6c72cbaf723ddc04184749d
Brian Lusina <lusinabrian@gmail.com>
2016-07-23 22:10:30 +0300

    Add digit count tests

---
+++ Math/DigitCount.java
@@ -0,0 +1,23 @@
+package Math;
+
+/**
+ * Package: Math
+ * Created by lusinabrian on 23/07/16 or 23 Jul of 2016,
+ * Time: 22:07.
+ * Project: JxProjects
+ * Description:
+ */
+public class DigitCount {
+    public static void main(String[] args){
+        System.out.println(nbDig(5750, 0));//, 4700);
+        System.out.printf("Actual: %d, Expected:%d",nbDig(11011, 2), 9481);
+        System.out.printf("Actual: %d, Expected:%d",nbDig(12224, 8), 7733);
+        System.out.printf("Actual: %d, Expected:%d",nbDig(11549, 1), 11905);
+    }
+
+    public static int nbDig(int n, int d) {
+        // your code
+    }
+
+/*CLASS EMD*/
+}
