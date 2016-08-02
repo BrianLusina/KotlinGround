386c7e38344b1cd175c5977bfba3b75506f8d8b3
Brian Lusina <lusinabrian@gmail.com>
2016-08-02 06:08:11 +0300

    Add CountBinary

---
+++ Binary/CountBinary.java
@@ -0,0 +1,11 @@
+package Binary;
+
+/**
+ * Package: Binary
+ * Created by lusinabrian on 02/08/16 or 02 Aug of 2016,
+ * Time: 06:06.
+ * Project: JxProjects
+ * Description:
+ */
+public class CountBinary {
+}
