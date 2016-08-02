7eb501d066213678722843ee70f0352fa972f548
Brian Lusina <lusinabrian@gmail.com>
2016-08-02 08:35:49 +0300

    add CombineString problem

---
+++ StringManipulation/CombineString.java
@@ -0,0 +1,18 @@
+package StringManipulation;
+
+/**
+ * Package: StringManipulation
+ * Created by lusinabrian on 02/08/16 or 02 Aug of 2016,
+ * Time: 08:30.
+ * Project: JxProjects
+ * Description:
+   We are given 3 strings: str1, str2, and str3.
+   Str3 is said to be a shuffle of str1 and str2 if it can be formed by interleaving the characters of str1 and str2
+   in a way that maintains the left to right ordering of the characters from each string.
+   For example, given str1="abc" and str2="def", str3="dabecf" is a valid shuffle since it preserves the character
+    ordering of the two strings.
+    So, given these 3 strings write a function that detects whether str3 is a valid shuffle of str1 and str2.
+
+ */
+public class CombineString {
+}
---
+++ test/StringManipulation/PhoneNumberTest.java
@@ -0,0 +1,50 @@
+package test.StringManipulation; 
+
+import junit.framework.Test; 
+import junit.framework.TestSuite; 
+import junit.framework.TestCase; 
+
+/** 
+* PhoneNumber Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>08/02/2016</pre> 
+* @version 1.0 
+*/ 
+public class PhoneNumberTest extends TestCase { 
+public PhoneNumberTest(String name) { 
+super(name); 
+} 
+
+public void setUp() throws Exception { 
+super.setUp(); 
+} 
+
+public void tearDown() throws Exception { 
+super.tearDown(); 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: createPhoneNumber(int[] numbers) 
+* 
+*/ 
+public void testCreatePhoneNumber() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+
+public static Test suite() { 
+return new TestSuite(PhoneNumberTest.class); 
+} 
+} 
