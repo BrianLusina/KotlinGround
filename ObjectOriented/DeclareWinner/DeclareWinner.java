81410137c891ea163368a0d7b208601827401251
Brian Lusina <lusinabrian@gmail.com>
2016-08-04 06:02:06 +0300

    Add Object Oriented tests

---
+++ ObjectOriented/DeclareWinner/DeclareWinner.java
@@ -0,0 +1,27 @@
+package ObjectOriented.DeclareWinner;
+
+/**
+ * Package: ObjectOriented.DeclareWinner
+ * Created by lusinabrian on 04/08/16 or 04 Aug of 2016,
+ * Time: 06:00.
+ * Project: JxProjects
+ * Description:
+ */
+public class DeclareWinner {
+
+    public class Fighter {
+        public String name;
+        public int health, damagePerAttack;
+        public Fighter(String name, int health, int damagePerAttack) {
+            this.name = name;
+            this.health = health;
+            this.damagePerAttack = damagePerAttack;
+        }
+    }
+
+    public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+        // Your code goes here. Have fun!
+    }
+
+/*class end*/
+}
---
+++ ObjectOriented/DeclareWinner/DeclareWinnerTest.java
@@ -0,0 +1,35 @@
+package ObjectOriented.DeclareWinner;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* DeclareWinner Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Aug 4, 2016</pre> 
+* @version 1.0 
+*/ 
+public class DeclareWinnerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) 
+* 
+*/ 
+@Test
+public void testDeclareWinner() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
