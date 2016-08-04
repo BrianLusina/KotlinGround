ebdb31361429685cb908b21b6fc47434ab9abad5
Brian Lusina <lusinabrian@gmail.com>
2016-08-04 05:43:24 +0300

    Create Tortoise Tests

---
+++ Math/TortoiseRacing/Tortoise.java
@@ -0,0 +1,15 @@
+package Math.TortoiseRacing;
+
+/**
+ * Package: Math.TortoiseRacing
+ * Created by lusinabrian on 04/08/16 or 04 Aug of 2016,
+ * Time: 05:36.
+ * Project: JxProjects
+ * Description:
+ */
+public class Tortoise {
+    public static int[] race(int v1, int v2, int g) {
+        // your code
+    }
+/*class end*/
+}
---
+++ Math/TortoiseRacing/TortoiseTest.java
@@ -0,0 +1,33 @@
+package Math.TortoiseRacing;
+
+import org.junit.Test; 
+import static org.junit.Assert.assertArrayEquals;
+
+/** 
+* Tortoise Tester. 
+* 
+* @author <Brian LUsina>
+* @since <pre>Aug 4, 2016</pre> 
+* @version 1.0 
+*/ 
+public class TortoiseTest { 
+    Tortoise tortoise = new Tortoise();
+
+    @Test
+    public void testRaceOne() throws Exception {
+        assertArrayEquals(new int[]{0, 32, 18}, Tortoise.race(720, 850, 70));
+
+    }
+
+    @Test
+    public void testRaceTwo() throws Exception{
+        assertArrayEquals(new int[]{3, 21, 49}, Tortoise.race(80, 91, 37));
+    }
+
+    @Test
+    public void testRaceThree() throws Exception{
+        assertArrayEquals(new int[]{2, 0, 0}, Tortoise.race(80, 100, 40));
+    }
+
+/*CLASS END*/
+}
