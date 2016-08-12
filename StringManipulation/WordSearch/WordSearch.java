ecccabad34e53a293498f20ca4261eb8ca366ac2
Brian Lusina <lusinabrian@gmail.com>
2016-08-12 06:14:42 +0300

    Add Word Search test class and solution class

---
+++ StringManipulation/WordSearch/WordSearch.java
@@ -0,0 +1,11 @@
+package StringManipulation.WordSearch;
+
+/**
+ * Package: StringManipulation.WordSearch
+ * Created by lusinabrian on 12/08/16 or 12 Aug of 2016,
+ * Time: 06:13.
+ * Project: JxProjects
+ * Description:
+ */
+public class WordSearch {
+}
---
+++ StringManipulation/WordSearch/WordSearchTest.java
@@ -0,0 +1,18 @@
+package StringManipulation.WordSearch;
+
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * WordSearch Tester.
+ *
+ * @author <Brian Lusina>
+ * @since <pre>Aug 12, 2016</pre>
+ * @version 1.0
+ */
+public class WordSearchTest {
+
+}
