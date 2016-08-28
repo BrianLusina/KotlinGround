226f501b06ceb73df558db0e03103887b57aec4a
Brian Lusina <lusinabrian@gmail.com>
2016-08-29 00:34:11 +0300

    Add coffee checkin package

---
+++ CoffeeCheckin/CoffeeCheckIn.java
@@ -0,0 +1,15 @@
+package CoffeeCheckin;
+
+/**
+ * Package: CoffeeCheckin
+ * Created by lusinabrian on 29/08/16 or 29 Aug of 2016,
+ * Time: 00:27.
+ * Project: JxProjects
+ * Description:
+ * - who was late
+ - who gets coffee as a result
+ - who is next in queue to get coffee the next time someone is late
+ */
+public class CoffeeCheckIn {
+
+}
