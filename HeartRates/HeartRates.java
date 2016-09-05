198963d96d6f90693800688baf23bf194c6ee76d
Brian Lusina <lusinabrian@gmail.com>
2016-09-05 06:44:50 +0300

    Add formatter

--- HeartRates/HeartRates.java
+++ HeartRates/HeartRates.java
@@ -1,3 +1,5 @@
+package HeartRates;
+
 import java.util.Scanner;
 
 public class HeartRates
---
+++ Strings/formatter.java
@@ -0,0 +1,19 @@
+package Strings;
+
+import java.util.Arrays;
+
+/**
+ * Package: Strings
+ * Created by lusinabrian on 05/09/16 or 05 Sep of 2016,
+ * Time: 06:25.
+ * Project: JxProjects
+ * Description:
+ */
+public class formatter {
+    public static String buildString(String... args) {
+            return "I like " + String.join(", ", args);
+    }
+    public static void main(String[] args){
+        System.out.println(buildString("Cheese", "Milk", "Bread","Steak"));
+    }
+}
