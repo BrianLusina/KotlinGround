d9702ea80f4ebb83a7d66445c0e3fbec859a0f9f
Brian Lusina <lusinabrian@gmail.com>
2016-09-06 05:54:57 +0300

    Add MinMax tests

--- Array/maxmin/MinMaxTest.java
+++ Array/maxmin/MinMaxTest.java
@@ -1,5 +1,11 @@
 package Array.maxmin;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+import static org.junit.Assert.assertArrayEquals;
+
 /**
  * Package: Array.maxmin
  * Created by lusinabrian on 06/09/16 or 06 Sep of 2016,
@@ -8,4 +14,26 @@ package Array.maxmin;
  * Description:
  */
 public class MinMaxTest {
+
+    Random rand;
+
+    @Before
+    public void initTest() {
+        rand = new Random();
+    }
+
+    @Test
+    public void testExampleCases() {
+        assertArrayEquals(new int[]{1,5}, MinMax.minMax(new int[]{1,2,3,4,5}));
+        assertArrayEquals(new int[]{5, 2334454}, MinMax.minMax(new int[]{2334454,5}));
+        assertArrayEquals(new int[]{1, 1}, MinMax.minMax(new int[]{1}));
+    }
+
+    @Test
+    public void minMaxRandomTest() {
+        for(int i = 0; i < 20; i++) {
+            int r = rand.nextInt();
+            assertArrayEquals(new int[]{r, r}, MinMax.minMax(new int[]{r}));
+        }
+    }
 }
