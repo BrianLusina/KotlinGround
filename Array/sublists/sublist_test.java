e73ffb4f02476c098a0cf2ac0240ce2568e3bb9e
Brian Lusina <lusinabrian@gmail.com>
2016-09-09 09:56:55 +0300

    Add sublist solution

---
+++ Array/sublists/sublist_test.java
@@ -0,0 +1,12 @@
+package Array.sublists;
+
+/**
+ * Package: Array.sublists
+ * Created by lusinabrian on 09/09/16 or 09 Sep of 2016,
+ * Time: 09:19.
+ * Project: JxProjects
+ * Description:
+ */
+public class sublist_test {
+
+}
