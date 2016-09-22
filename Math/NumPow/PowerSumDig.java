74e94a59f2279b5105a41cb478af515d7fefe2ac
Brian Lusina <lusinabrian@gmail.com>
2016-09-22 06:10:37 +0300

    Add power num digits

--- Math/NumPow/PowerSumDig.java
+++ Math/NumPow/PowerSumDig.java
@@ -1,7 +1,21 @@
 package Math.NumPow;
 
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
 public class PowerSumDig {
+    /**
+     * Get each digit of the number and sum it
+     * Converts the number to a string, determines length and raises each digit to the power of that length
+     * sums the output
+     *
+     * Check if both are equal, add to a list
+     * */
     public static long powerSumDigTerm(int n) {
-        // your code
+        int output = 0;
+        ArrayList<Integer> nums = new ArrayList<>();
+        for(Integer number : IntStream.range(1,10).toArray()){
+
+        }
     }
 }
