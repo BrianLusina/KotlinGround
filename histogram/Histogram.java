3017772f401e56ae1e318508f73dcae528de8ceb
Brian Lusina <lusinabrian@gmail.com>
2016-09-22 06:32:41 +0300

    Add histogram solution

--- histogram/Histogram.java
+++ histogram/Histogram.java
@@ -1,7 +1,20 @@
 package histogram;
 
 public class Histogram {
+    /***
+     * Loop through the string converting the loop count to a string while concatenating
+     * the count of hashes and the number itself.
+     * @param results the integer array with the roll results, They will be arranged in order the 1st element is
+     * the roll count of number 1 on the die and so on.
+     * @return the histogram in string format*/
     public static String histogram(final int results[]) {
-        return "your histogram goes here";
+        String output = "";
+        for(int i =0; i < results.length;i++) {
+            output += String.valueOf(i+1) + "|"
+                    + new String(new char[results[i]]).replace("\0","#")
+                    + " " + String.valueOf(results[i]) + "\n";
+        }
+
+        return output;
     }
 }
