7dbf3e6eec87585145887308c0bbf075684bdc1d
BrianLusina <lusinabrian@gmail.com>
2016-10-26 12:25:32 +0300

    Add test cases for averages

--- Math/AveragesOfNumbers/Averages.java
+++ Math/AveragesOfNumbers/Averages.java
@@ -1,12 +1,9 @@
 package Math.AveragesOfNumbers;
 
-/**
- * Math
- * Created by lusinabrian on 26/10/16.
- * Description:
- */
-public class Averages {
-    public static double[] averages(int[] numbers) {
+class Averages {
+
+    static double[] averages(int[] numbers) {
         return new double[0];
+
     }
 }
---
+++ Math/AveragesOfNumbers/AveragesTests.java
@@ -0,0 +1,19 @@
+package Math.AveragesOfNumbers;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+import java.util.Arrays;
+
+/**
+ * Math.AveragesOfNumbers
+ * Created by lusinabrian on 26/10/16.
+ * Description: Tests for {@link Averages}
+ */
+public class AveragesTests {
+    @Test
+    public void basicTests(){
+        assertEquals(Arrays.toString(new double[] { 2, 2, 2, 2 }), Arrays.toString(Averages.averages(new int[] { 2, 2, 2, 2, 2 })));
+        assertEquals(Arrays.toString(new double[] { 0, 0, 0, 0 }), Arrays.toString(Averages.averages(new int[] { 2, -2, 2, -2, 2 })));
+        assertEquals(Arrays.toString(new double[] { 2, 4, 3, -4.5 }), Arrays.toString(Averages.averages(new int[] { 1, 3, 5, 1, -10 })));
+    }
+}
