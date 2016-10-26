a7da1df9ea099a5ebdba048b2bc8d71c719e3510
BrianLusina <lusinabrian@gmail.com>
2016-10-26 12:22:18 +0300

    Add averages of numbers

---
+++ Math/AveragesOfNumbers/Averages.java
@@ -0,0 +1,12 @@
+package Math.AveragesOfNumbers;
+
+/**
+ * Math
+ * Created by lusinabrian on 26/10/16.
+ * Description:
+ */
+public class Averages {
+    public static double[] averages(int[] numbers) {
+        return new double[0];
+    }
+}
