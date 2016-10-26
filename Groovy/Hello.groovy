9d8c0422f5052d9ebbff7081def51188c7082aba
BrianLusina <lusinabrian@gmail.com>
2016-10-26 12:48:39 +0300

    Add solution for average problem

--- Groovy/Hello.groovy
+++ Groovy/Hello.groovy
@@ -4,5 +4,7 @@ class Example {
     static void main(String[] args) {
         // Using a simple println statement to print output to the console
         println 'Hello World'
+
+        println((-10 + 1)/2);
     }
 }
--- Math/AveragesOfNumbers/Averages.java
+++ Math/AveragesOfNumbers/Averages.java
@@ -3,7 +3,15 @@ package Math.AveragesOfNumbers;
 class Averages {
 
     static double[] averages(int[] numbers) {
-        return new double[0];
+        if(numbers == null || numbers.length == 0){
+            return new double[]{};
+        }
 
+        double[] out = new double[numbers.length -1];
+        for(int x = 0; x < numbers.length-1; x++){
+            out[x] = ((double) numbers[x] + (double) numbers[x+1]) / 2;
+        }
+
+        return out;
     }
 }
