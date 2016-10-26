1413833d019b4d4f18697d5239c563ae4d4ba957
BrianLusina <lusinabrian@gmail.com>
2016-10-26 13:11:38 +0300

    Add deadants incomplete solution

--- Puzzles/DeadAnts/Deadants.java
+++ Puzzles/DeadAnts/Deadants.java
@@ -1,11 +1,26 @@
 package Puzzles.DeadAnts;
 
 class Deadants {
+    /**
+     * Checks if the argument is null, returns 0
+     * creates a string to later check if the word is ant or a regular word
+     * count the number of words in the string using a regex*/
     static int deadAntCount(final String ants) {
+        String ant = "ant|^([a-zA-Z]*)$";
+
         if(ants == null){
             return 0;
         }
-        
-        return 0;
+
+        String[] stampede = ants.split(" ");
+        int count = 0;
+
+        for (String aStampede : stampede) {
+            
+            if (!aStampede.equals("ant")) {
+                count++;
+            }
+        }
+        return count;
     }
 }
