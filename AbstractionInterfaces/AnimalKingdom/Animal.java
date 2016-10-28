6b4da19453f7f76cc5dcdacba18ad042549d79eb
BrianLusina <lusinabrian@gmail.com>
2016-10-28 10:32:46 +0300

    Add Abstraction and interfaces package

---
+++ AbstractionInterfaces/AnimalKingdom/Animal.java
@@ -0,0 +1,13 @@
+package AbstractionInterfaces.AnimalKingdom;
+
+/**
+ * AbstractionInterfaces
+ * Created by lusinabrian on 28/10/16.
+ * Description:Abstract class for the animal kingdom
+ */
+abstract class Animal {
+
+    public void eat(){}
+
+    public void sleep(int hours)
+}
