327d12d0a5344abaa3a165f7c34b9a663f2af5fc
BrianLusina <lusinabrian@gmail.com>
2016-10-31 00:12:46 +0300

    Add Groovy Basics

--- Groovy/Hello.groovy
+++ Groovy/Hello.groovy
@@ -8,9 +8,36 @@ class Example {
         def x = 42
         println x.getClass()
 
-        // change the variable type
+        // change the variable type at runtime
         x = "Hello"
         println x.getClass()
-        
+
+        // String implementation using GString
+        def str = "Lusina"
+        println "Hello, $str"
+
+        // more complex string statements, which allow accessing strings like accessing arrays
+        def firstName = "Brian"
+        def lastName = "Lusina"
+        println "My name is ${firstName[0]}. $lastName"
+
+        //implicit truthy
+        if(x){
+            println true
+        }
+
+        if(!str){
+            println(true)
+        }
+
+        // regular expressions in Groovy start with ~ at the beginning and are enclosed in //
+        def pattern = ~/[a-zA-Z]/
+        println pattern.getClass()
+
+        def matcher = "The Hitchhiker's Guide to the Galaxy" =~ /Galaxy/
+        if (matcher) {
+            println "Found the word 'Galaxy'"
+        }
+
     }
 }
