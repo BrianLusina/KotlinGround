29e0cb8958f294f70394000757db88c8205bd29d
BrianLusina <lusinabrian@gmail.com>
2016-10-30 23:52:03 +0300

    Add groovy variables

--- Groovy/Hello.groovy
+++ Groovy/Hello.groovy
@@ -4,5 +4,13 @@ class Example {
     static void main(String[] args) {
         // Using a simple println statement to print output to the console
         println 'Hello World'
+        // defining variables
+        def x = 42
+        println x.getClass()
+
+        // change the variable type
+        x = "Hello"
+        println x.getClass()
+        
     }
 }
