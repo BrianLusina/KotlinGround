1e3eb006c8182340452aae424ebe4f7b53d216c6
BrianLusina <lusinabrian@gmail.com>
2016-11-18 15:17:12 +0300

    Add hashmap to evaluate who is next

--- CoffeeCheckin/CoffeeCheckInSimulation.java
+++ CoffeeCheckin/CoffeeCheckInSimulation.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Collections;
-import CoffeeCheckin.CoffeeCheckinMarkers;
+import static CoffeeCheckin.CoffeeCheckinMarkers.*;
+
 
 public class CoffeeCheckInSimulation {
 
     public static void main(String[] args){
         System.out.println("Hello, welcome to Coffee Check-in, please enter list of employees for the week( separate names with a space)");
         Scanner scannerEmplInput = new Scanner(System.in);
-        employee_queue(scannerEmplInput.nextLine());
+        String employeeList = scannerEmplInput.nextLine();
+
+        //add to queue
+        employee_queue(employeeList);
 
         System.out.println("Who was late today?(Separate names with spaces)");
         String late_comers = scannerEmplInput.nextLine();
@@ -19,20 +23,29 @@ public class CoffeeCheckInSimulation {
         //late_evaluater(employee_queue(employee_list), late_comers);
     }
 
-    /**queues the list of employees and add a 'marker', specifying who is next in the list*/
-    private static ArrayList<String> employee_queue(String employee_list) {
-        HashMap<String, Enum> employeeMap = new HashMap<>();
-        ArrayList<String> list = new ArrayList<>();
-        Collections.addAll(list, employee_list.split(" "));
-        return list;
+    /**queues the list of employees and add a 'marker', specifying who is next in the list
+     * @param employeeListStr the employee list input as a string
+     * */
+    private static HashMap<String, Enum> employee_queue(String employeeListStr) {
+        //first name on the list is assumed to be next
+        HashMap<String, Enum> employeeHash = new HashMap<>();
+        String[] employeeList = employeeListStr.split(" ");
+
+        employeeHash.put(employeeList[0], NEXT);
+        //add the rest of the names
+        for(int x = 1; x < employeeList.length; x++){
+            employeeHash.put(employeeList[x], OTHER);
+        }
+        return employeeHash;
     }
 
-    /**Takes in initial list and new string with late comers and evaluates who will buy coffee*/
+    /**Takes in initial list and new string with late comers and evaluates who will buy coffee
+     * @param queue the employee queue for the week
+     * @param late_comers The list of late comers for the day*/
     private static ArrayList<String> late_evaluater(ArrayList<String> queue, String late_comers) {
         ArrayList<String> updated_list = new ArrayList<>();
         for(String late: late_comers.split(" ")){
             for(String q: queue){
-
             }
         }
 
--- CoffeeCheckin/CoffeeCheckinMarkers.java
+++ CoffeeCheckin/CoffeeCheckinMarkers.java
@@ -3,9 +3,9 @@ package CoffeeCheckin;
 /**
  * CoffeeCheckin
  * Created by lusinabrian on 18/11/16.
- * Description:
+ * Description: Markers used in hashes to evaluate the employees
  */
 enum CoffeeCheckinMarkers {
-    LATE, NEXT,
+    LATE, NEXT, OTHER,
     MONDAY,TUESDAY, WEDNESDAY, THURSDAY, FRIDAY
 }
