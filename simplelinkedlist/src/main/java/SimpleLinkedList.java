aea5d74605c962985647423e3a17532c6dbdc29c
BrianLusina <lusinabrian@gmail.com>
2016-11-28 11:39:43 +0300

    add simple linked list

---
+++ simplelinkedlist/src/main/java/SimpleLinkedList.java
@@ -0,0 +1,24 @@
+package simplelinkedlist.src.main.java;
+
+public class SimpleLinkedList<T> {
+
+    public SimpleLinkedList(){}
+
+    public SimpleLinkedList(Integer[] values){
+
+    }
+
+    public int size(){
+
+    }
+
+    public void pop(){}
+
+    public void push(int element){}
+
+    public void reverse(){}
+
+    public T[] asArray(Class<T> clazz){
+
+    }
+}
---
+++ simplelinkedlist/src/test/java/SimpleLinkedListTest.java
@@ -0,0 +1,85 @@
+package simplelinkedlist.src.test.java;
+
+
+import java.util.NoSuchElementException;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.Ignore;
+import simplelinkedlist.src.main.java.SimpleLinkedList;
+
+
+public class SimpleLinkedListTest {
+
+
+    @Test
+    public void aNewListIsEmpty() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        assertThat(list.size()).isEqualTo(0);
+    }
+
+    @Ignore
+    @Test
+    public void canCreateFromArray() {
+        Integer[] values = new Integer[]{1, 2, 3};
+        SimpleLinkedList list = new SimpleLinkedList(values);
+        assertThat(list.size()).isEqualTo(3);
+    }
+
+    @Ignore
+    @Test(expected = NoSuchElementException.class)
+    public void popOnEmptyListWillThrow() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.pop();
+    }
+
+    @Ignore
+    @Test
+    public void popReturnsLastAddedElement() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.push(9);
+        list.push(8);
+        assertThat(list.size()).isEqualTo(2);
+        assertThat(list.pop()).isEqualTo(8);
+        assertThat(list.pop()).isEqualTo(9);
+        assertThat(list.size()).isEqualTo(0);
+    }
+
+    @Ignore
+    @Test
+    public void reverseReversesList() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.push(9);
+        list.push(8);
+        list.push(7);
+        list.push(6);
+        list.push(5);
+        list.reverse();
+        assertThat(list.pop()).isEqualTo(9);
+        assertThat(list.pop()).isEqualTo(8);
+        assertThat(list.pop()).isEqualTo(7);
+        assertThat(list.pop()).isEqualTo(6);
+        assertThat(list.pop()).isEqualTo(5);
+    }
+
+    @Ignore
+    @Test
+    public void canReturnListAsArray() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        list.push(9);
+        list.push(8);
+        list.push(7);
+        list.push(6);
+        list.push(5);
+        Integer[] expected = {5, 6, 7, 8, 9};
+        assertThat(list.asArray(Integer.class)).containsExactly(expected);
+    }
+
+    @Ignore
+    @Test
+    public void canReturnEmptyListAsEmptyArray() {
+        SimpleLinkedList list = new SimpleLinkedList();
+        Object[] expected = {};
+        assertThat(list.asArray(Object.class)).containsExactly(expected);
+    }
+
+}
