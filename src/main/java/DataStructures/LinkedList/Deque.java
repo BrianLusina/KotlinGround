a02c431eedfb02cb18334c70f8f520ab025adf36
BrianLusina <lusinabrian@gmail.com>
2016-12-10 20:51:50 +0300

    add deque solution

--- src/main/java/DataStructures/LinkedList/Deque.java
+++ src/main/java/DataStructures/LinkedList/Deque.java
@@ -1,33 +1,59 @@
 package main.java.DataStructures.LinkedList;
 
 public class Deque<T> {
+    private Element<T> head;
 
+    public void push(T value) {
+        if (head == null) {
+            head = new Element<>(value, null, null);
+            head.next = head;
+            head.prev = head;
+            return;
+        }
 
-    public final void push(T elem) {
-
+        Element<T> oldTail = head.prev;
+        Element<T> tail = new Element<>(value, oldTail, head);
+        oldTail.next = tail;
+        head.prev = tail;
     }
 
-    public final T pop() {
-        return null;
+    public T pop() {
+        head = head.prev;
+        return shift();
     }
 
-    public final T shift() {
-        return null;
+    public void unshift(T value) {
+        push(value);
+        head = head.prev;
     }
 
-    public final void unshift(T value) {
+    public T shift() {
+        T value = head.value;
 
+        Element<T> newHead = head.next;
+        Element<T> newTail = head.prev;
+
+        if (newHead == head) {
+            head = null;
+        }
+        else {
+            newHead.prev = newTail;
+            newTail.next = newHead;
+            head = newHead;
+        }
+
+        return value;
     }
 
-    private static class Element<T> {
-        final T value;
-        Element next;
-        Element prev;
+    private static final class Element<T> {
+        private final T value;
+        private Element<T> prev;
+        private Element<T> next;
 
-        Element(T value) {
+        public Element(T value, Element<T> prev, Element<T> next) {
             this.value = value;
+            this.prev = prev;
+            this.next = next;
         }
     }
-
-
 }
