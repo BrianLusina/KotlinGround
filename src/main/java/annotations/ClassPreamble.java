ef1f2915edfaabf59de5064e11c8d8b25c625242
BrianLusina <lusinabrian@gmail.com>
2016-12-25 01:52:17 +0300

    add annotations

---
+++ src/main/java/annotations/ClassPreamble.java
@@ -0,0 +1,20 @@
+package annotations;
+
+import java.lang.annotation.Documented;
+
+/**
+ * annotations
+ * Created by lusinabrian on 25/12/16.
+ * Description: preamble Annotation for each class
+ */
+
+@Documented
+public @interface ClassPreamble{
+    String author();
+    String date();
+    int currentRevision() default 1;
+    String lastModified() default "N/A";
+    String lastModifiedBy() default "N/A";
+    // Note use of array
+    String[] reviewers();
+}
---
+++ src/main/java/annotations/FancyClass.java
@@ -0,0 +1,17 @@
+package annotations;
+
+/**
+ * annotations
+ * Created by lusinabrian on 25/12/16.
+ * Description:
+ */
+@ClassPreamble(
+        author = "Brian Lusina",
+        date = "",
+        currentRevision = 1,
+        lastModified = "",
+        lastModifiedBy = "Brian Lusina",
+        reviewers = {"Brian Lusina"}
+)
+public class FancyClass {
+}
