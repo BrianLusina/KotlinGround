35c9021d37d1ee4d15f50347ea96f7940d317705
BrianLusina <lusinabrian@gmail.com>
2016-12-25 12:14:15 +0300

    add ordered pair

---
+++ src/main/java/generics/orderedpair/OrderedPair.java
@@ -0,0 +1,26 @@
+package generics.orderedpair;
+
+/**
+ * generics.orderedpair
+ * Created by lusinabrian on 25/12/16.
+ * Description:
+ */
+public class OrderedPair<K, V> implements Pair<K, V>{
+    private K key;
+    private V value;
+
+    public OrderedPair(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/generics/orderedpair/Pair.java
@@ -0,0 +1,12 @@
+package generics.orderedpair;
+
+/**
+ * generics.orderedpair
+ * Created by lusinabrian on 25/12/16.
+ * Description:
+ */
+public interface Pair<K, V> {
+    public K getKey();
+
+    public V getValue();
+}
