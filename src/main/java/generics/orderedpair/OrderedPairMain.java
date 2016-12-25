865e48a93581b95f9258e5850714c0386e178dd6
BrianLusina <lusinabrian@gmail.com>
2016-12-25 12:17:58 +0300

    add ordered pair main

---
+++ src/main/java/generics/orderedpair/OrderedPairMain.java
@@ -0,0 +1,14 @@
+package generics.orderedpair;
+
+/**
+ * generics.orderedpair
+ * Created by lusinabrian on 25/12/16.
+ * Description:
+ */
+public class OrderedPairMain {
+    public static void main(String[] args){
+        Pair<String, Integer> p1 = new OrderedPair<>("Brian", 5);
+        Pair<String, String> p2 = new OrderedPair<>("Lusina", "Ombito");
+        
+    }
+}
