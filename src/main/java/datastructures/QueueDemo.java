2a091d1ff60129fb542320ddf4409594957e57da
BrianLusina <lusinabrian@gmail.com>
2016-12-26 17:15:12 +0300

    add queue adt

--- src/main/java/datastructures/QueueDemo.java
+++
@@ -1,7 +0,0 @@
-package datastructures.DataTypes;
-
-/**
- * Created by lusinabrian on 12/07/16.
- */
-public class QueueDemo {
-}
---
+++ src/main/java/datastructures/queues/Queues.java
@@ -0,0 +1,10 @@
+package datastructures.queues;
+import java.util.Queue;
+
+/**
+ * datastructures.queues
+ * Created by lusinabrian on 26/12/16.
+ * Description:
+ */
+public class Queues {
+}
