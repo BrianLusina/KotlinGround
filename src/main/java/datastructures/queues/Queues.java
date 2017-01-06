40022635a2c3632cc74f4b3aa2360248d0963ad6
BrianLusina <lusinabrian@gmail.com>
2017-01-06 10:34:44 +0300

    add queue implementation

--- src/main/java/datastructures/queues/Queues.java
+++ src/main/java/datastructures/queues/Queues.java
@@ -4,7 +4,36 @@ import java.util.Queue;
 /**
  * datastructures.queues
  * Created by lusinabrian on 26/12/16.
- * Description:
+ * Description: Implementation of a queue data structure
+ *
  */
-public class Queues {
+public class Queues<T> implements QueueInterface{
+    private Queue<Object> queue;
+
+    private Queues(){}
+
+    @Override
+    public void enqueue(Object element) {
+        queue.add(element);
+    }
+
+    @Override
+    public Object dequeue() {
+        return queue.remove();
+    }
+
+    @Override
+    public Object peek() {
+        return queue.peek();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
 }
---
+++ src/test/java/datastructures/queues/QueuesTest.java
@@ -0,0 +1,19 @@
+package datastructures.queues;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class QueuesTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
