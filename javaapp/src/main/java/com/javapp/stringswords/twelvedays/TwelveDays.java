1daaebbeaab8aa91527acf6534a63d89435c35ae
BrianLusina <lusinabrian@gmail.com>
2017-03-08 10:16:16 +0300

    add twelve days verses

---
+++ javaapp/src/main/java/com/javapp/stringswords/twelvedays/TwelveDays.java
@@ -0,0 +1,29 @@
+package com.javapp.stringswords.twelvedays;
+
+
+public class TwelveDays {
+    /**
+     * Searches for a particular verse give the verse number and returns it
+     * @param verseNo the verse number
+     * @return the verse at this particular verse number*/
+    public static String verse(int verseNo){
+
+    }
+
+    /**
+     * Prints out the verse from start to end given the starting verse number and ending verse number
+     * @param start starting verse as an integer
+     * @param end ending verse as an integer
+     * @return the verses from the start verse to the end verse
+     * */
+    public static String verses(int start, int end){
+
+    }
+
+    /**
+     * Sings out the whole 12 days song
+     *@return the whole song*/
+    public static String sing(){
+
+    }
+}
