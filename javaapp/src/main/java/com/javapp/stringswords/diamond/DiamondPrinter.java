af2f03b66e594fbb2e5c142e738442898477283f
BrianLusina <lusinabrian@gmail.com>
2017-03-13 10:14:59 +0300

    add diamond test cases

---
+++ javaapp/src/main/java/com/javapp/stringswords/diamond/DiamondPrinter.java
@@ -0,0 +1,16 @@
+package com.javapp.stringswords.diamond;
+
+import java.util.List;
+
+public class DiamondPrinter {
+
+    /**
+     *
+     * @param letter letter to print out a diamond
+     * @return A list which will represent a diamond
+     * */
+    public List<String> printToList(char letter){
+
+    }
+
+}
