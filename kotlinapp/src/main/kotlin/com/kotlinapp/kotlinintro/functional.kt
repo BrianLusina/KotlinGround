87d25d8656393cac271ba8b9a7dce82af75106ac
Brian Lusina <lusinabrian@gmail.com>
2017-05-28 11:08:25 +0300

    functional programming with .flatMap

--- kotlinapp/src/main/kotlin/com/kotlinapp/kotlinintro/functional.kt
+++ kotlinapp/src/main/kotlin/com/kotlinapp/kotlinintro/functional.kt
@@ -11,23 +11,28 @@ import java.security.Security
 fun main(args: Array<String>) {
     val providers = Providers()
     val details = providers.getAllProviders()
+    val filteredData = providers.getFilteredProviders("random")
+    val filteredDataFlatMap = providers.getFilteredProvidersEx("Random")
+
+    filteredData.forEach(::println)
+    filteredDataFlatMap.forEach(::println)
 
     // 3 ways to do this
     // 1
-    details.forEach{
-        entry -> println("ProviderName: ${entry.providerName}, name: ${entry.name}")
-    }
-
-    // using it
-    details.forEach{
-        println("ProviderName: ${it.providerName}, name: ${it.name}")
-    }
-
+//    details.forEach{
+//        entry -> println("ProviderName: ${entry.providerName}, name: ${entry.name}")
+//    }
+//
+//    // using it
+//    details.forEach{
+//        println("ProviderName: ${it.providerName}, name: ${it.name}")
+//    }
+//
     // using a function reference
-    details.forEach(::println)
+//    details.forEach(::println)
 
     // passing in println function
-    getAllProviders { key -> println("Provider: $key") }
+  //  getAllProviders { key -> println("Provider: $key") }
 }
 
 /**
@@ -68,6 +73,48 @@ class Providers{
         }
 
         return listOfProviders
+    }
+
+    /**
+     * Returns a list of filtered provider details
+     * @param filter what to filter
+     * */
+    fun getFilteredProviders(filter: String): List<ProviderDetails>{
+        val providers = Security.getProviders()
+        val listOfProviders = mutableListOf<ProviderDetails>()
+
+        // iterate over providers and create provider details from it
+        providers.forEach {
+            provider ->
+            val providerDetails = provider.entries.filter{
+                // check if the ke has the filter and ignore case
+                it -> it.key.toString().contains(filter, true)
+            }.map {
+                ProviderDetails(provider.name, it.key.toString())
+            }
+            listOfProviders += providerDetails
+        }
+
+        return listOfProviders
+    }
+    /**
+     * Returns a list of filtered provider details using flatMap
+     * @param filter what to filter
+     * */
+    fun getFilteredProvidersEx(filter: String): List<ProviderDetails>{
+        val providers = Security.getProviders()
+
+        // iterate over providers and create provider details from it
+        return providers.flatMap {
+            provider ->
+                provider.entries.filter{
+                    // check if the ke has the filter and ignore case
+                    it -> it.key.toString().contains(filter, true)
+                }.map {
+                    ProviderDetails(provider.name, it.key.toString())
+                }
+            }
+
     }
 
     // this allows calling this function without creating an instance of Providers class
