4397dacf308b78715a9d495d0991524dff7b1ba7
Brian Lusina <lusinabrian@gmail.com>
2017-05-28 08:23:14 +0300

    create classes demo in kotlin

---
+++ kotlinapp/src/main/kotlin/com/kotlinapp/kotlinintro/classes.kt
@@ -0,0 +1,9 @@
+package com.kotlinapp.kotlinintro
+
+/**
+ * @author lusinabrian on 28/05/17.
+ * Simple demo on classes in Kotlin
+ */
+
+class Animal constructor(firstName: String, species: String)
+
---
+++ kotlinapp/src/main/kotlin/com/kotlinapp/objects/com/MyProviders.kt
@@ -0,0 +1,7 @@
+package com.kotlinapp.objects.com
+
+/**
+ * @author lusinabrian on 26/05/17.
+ */
+class MyProviders {
+}
