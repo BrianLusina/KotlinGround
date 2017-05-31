53d4ef5664ec895d6ea2ffe43b4f416cd8ef5791
Brian Lusina <lusinabrian@gmail.com>
2017-05-31 20:14:46 +0300

    crete etl test cases

---
+++ kotlinapp/src/main/kotlin/com/kotlinapp/algorithms/etl/ETL.kt
@@ -0,0 +1,15 @@
+package com.kotlinapp.algorithms.etl
+
+/**
+ * @author lusinabrian on 31/05/17.
+ */
+class ETL{
+    companion object{
+        /**
+         * Maps the old data to a new system
+         * */
+        fun transform(oldMap : Map<Int, List<Char>>) : HashMap<String, Int>{
+
+        }
+    }
+}
