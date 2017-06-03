c9694190c2a66c69e6213aa047230e28007f4827
Brian Lusina <lusinabrian@gmail.com>
2017-06-03 11:32:57 +0300

    create word count test cases

---
+++ kotlinapp/src/main/kotlin/com/kotlinapp/ktstrings/wordcount/WordCount.kt
@@ -0,0 +1,11 @@
+package com.kotlinapp.ktstrings.wordcount
+
+/**
+ * @author lusinabrian on 03/06/17.
+ * Counts the number of words in a phrase
+ */
+object WordCount{
+    fun phrase(sentence: String): Map<String, Int>{
+
+    }
+}
