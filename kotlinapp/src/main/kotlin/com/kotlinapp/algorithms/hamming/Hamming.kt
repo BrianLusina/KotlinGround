8a6b01f6f4ad6e038e2f47baa3fc9e9755cef2be
Brian Lusina <lusinabrian@gmail.com>
2017-07-30 12:28:51 +0300

    update kotlin hamming variation

--- kotlinapp/src/main/kotlin/com/kotlinapp/algorithms/hamming/Hamming.kt
+++ kotlinapp/src/main/kotlin/com/kotlinapp/algorithms/hamming/Hamming.kt
@@ -5,6 +5,14 @@ package com.kotlinapp.algorithms.hamming
  */
 object Hamming {
     fun compute(strandA: String, strandB: String): Int {
-
+        var count = 0
+        if(strandA.length != strandB.length){
+            throw IllegalArgumentException("leftStrand and rightStrand must be of equal length.")
+        }
+        strandA.forEachIndexed { index, c ->
+            if( strandB[index] != c)
+                count += 1
+        }
+        return count
     }
 }
