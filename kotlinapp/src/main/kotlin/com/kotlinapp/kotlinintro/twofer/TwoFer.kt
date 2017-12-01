db00e385e34b82855fc2bf8c35f0f30d9e48629e
Brian Lusina <lusinabrian@gmail.com>
2017-12-01 11:00:30 +0300

    2nd iteration on twofer

--- kotlinapp/src/main/kotlin/com/kotlinapp/kotlinintro/twofer/TwoFer.kt
+++ kotlinapp/src/main/kotlin/com/kotlinapp/kotlinintro/twofer/TwoFer.kt
@@ -4,4 +4,4 @@ package com.kotlinapp.kotlinintro.twofer
  * @author lusinabrian on 21/11/17.
  */
 
-fun twofer(vararg name: String) : String = "One for ${if(name.isEmpty()) "you" else name[0]}, one for me."
+fun twofer(name: String = "you") = "One for $name, one for me."
