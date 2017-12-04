282ba7e4a01d7c73cf3bf60acb5e4fcbc904964d
Brian Lusina <lusinabrian@gmail.com>
2017-12-04 12:07:42 +0300

    Scrabble Score in kotlin

---
+++ kotlinapp/src/main/kotlin/com/kotlinapp/puzzles/scrabblescore/ScrabbleScore.kt
@@ -0,0 +1,29 @@
+package com.kotlinapp.puzzles.scrabblescore
+
+/**
+ * @author lusinabrian on 04/12/17.
+ */
+object ScrabbleScore {
+
+    private val scoreMap = mapOf(
+            arrayListOf("A", "E", "I", "O", "U", "L", "N", "R", "S", "T") to 1,
+            arrayListOf("D", "G") to 2,
+            arrayListOf("B", "C", "M", "P") to 3,
+            arrayListOf("F", "H", "V", "W", "Y") to 4,
+            arrayListOf("K") to 5,
+            arrayListOf("J", "X") to 8,
+            arrayListOf("Q", "Z") to 10
+    )
+
+    fun scoreWord(word: String): Int {
+        var total = 0
+        val letters = word.toUpperCase().split("")
+
+        scoreMap.forEach { arrList, score ->
+            letters.filter { arrList.contains(it) }
+                    .forEach { total += score }
+        }
+        // return the total
+        return total
+    }
+}
