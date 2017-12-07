64ba7e092635cb6ee792df15acdd966801a64c92
Brian Lusina <lusinabrian@gmail.com>
2017-12-07 18:11:06 +0300

    2nd iteration on square test

--- app/src/main/kotlin/com/kotlinapp/ktmath/diffOfSquares/Squares.kt
+++ app/src/main/kotlin/com/kotlinapp/ktmath/diffOfSquares/Squares.kt
@@ -1,12 +1,11 @@
 class Squares(val n: Int) {
     fun squareOfSum(): Int {
-        val naturalNumbers = IntRange(1, n)
-        return Math.pow(naturalNumbers.sum().toDouble(), 2.0).toInt()
+        val sumOfNNaturalNums = (1..n).sum()
+        return sumOfNNaturalNums * sumOfNNaturalNums
     }
 
     fun sumOfSquares(): Int {
-        val naturalNumbers = IntRange(1, n)
-        return naturalNumbers.reduce{acc, i ->  acc + (i * i)}
+        return (1..n).reduce{acc, i ->  acc + (i * i)}
     }
 
     fun difference() = squareOfSum() - sumOfSquares()
