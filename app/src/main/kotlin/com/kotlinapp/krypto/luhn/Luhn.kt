6a0f63b9cec1b0214be771f30d2e7509c13f3031
Brian Lusina <12752833+brianlusina@users.noreply.github.com>
2018-03-13 21:46:45 +0300

    Second variation of Luhn Implementation
    Signed-off-by: Brian Lusina <12752833+BrianLusina@users.noreply.github.com>

--- app/src/main/kotlin/com/kotlinapp/krypto/luhn/Luhn.kt
+++ app/src/main/kotlin/com/kotlinapp/krypto/luhn/Luhn.kt
@@ -5,7 +5,7 @@ package com.kotlinapp.krypto.luhn
  */
 object Luhn {
 
-    fun isValid(cardNumber : String) : Boolean {
+    fun isValid(cardNumber: String): Boolean {
         val newCardNumber = cardNumber.replace(" ", "")
 
         if (newCardNumber.length <= 1) {
@@ -18,6 +18,38 @@ object Luhn {
 
         return (evenlyPositioned + oddlyPositioned) % 10 == 0
 
+    }
+
+    /**
+     * Second variation of isValid
+     */
+    fun isValid2(number: String): Boolean {
+        val code = number.replace(" ", "")
+
+        // if not number with 2 or more digits
+        if (!code.matches(Regex("^\\d{2,}$"))) {
+            return false
+        }
+
+        val sum = code.reversed().map { Character.getNumericValue(it) }
+                .mapIndexed { index, num ->
+                    if (index % 2 == 0) {
+                        doubleDigit(num)
+                    } else {
+                        num
+                    }
+                }.sum()
+
+        return sum % 10 == 0
+    }
+
+    private fun doubleDigit(n: Int): Int {
+        val res = n * 2
+        return if (res > 9) {
+            res - 9
+        } else {
+            res
+        }
     }
 
     private fun sumDigits(n: Int) = n / 10 + n % 10
