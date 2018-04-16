7413fefbbb073598832087205c9e07309aac8793
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-04-16 22:21:50 +0300

    sieve or erastothenes

---
+++ app/src/main/kotlin/com/kotlinapp/ktmath/sieve/Sieve.kt
@@ -0,0 +1,25 @@
+package com.kotlinapp.ktmath.sieve
+
+/**
+ * @author lusinabrian on 16/04/18.
+ */
+object Sieve {
+
+    fun primesUpTo(ceil : Int) : List<Int>{
+        if (ceil == 1){
+            return emptyList()
+        }
+
+        var possiblePrimes = (2..ceil).toList()
+        val primes = mutableListOf<Int>()
+
+        do {
+            val prime = possiblePrimes[0]
+            primes.add((prime))
+
+            possiblePrimes = possiblePrimes.filter { it % prime != 0 }.toMutableList()
+        } while (!possiblePrimes.isEmpty())
+
+        return primes
+    }
+}
