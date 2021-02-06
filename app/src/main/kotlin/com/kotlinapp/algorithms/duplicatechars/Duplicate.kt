778632134183a43bc77252c98447f1927f88a642
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-02-06 12:24:57 +0300

    feat(algorithms): contains duplicates
    checks if 2 strings contain duplicate characters

---
+++ app/src/main/kotlin/com/kotlinapp/algorithms/duplicatechars/Duplicate.kt
@@ -0,0 +1,12 @@
+object Duplicate {
+    fun sortString(word: String): String {
+        return word.toCharArray().sorted().joinToString("")
+    }
+
+    fun containsDuplicates(wordOne: String, wordTwo: String): Boolean {
+        if (wordOne.length != wordTwo.length){
+            return false
+        }
+        return sortString(wordOne) == sortString(wordTwo)
+    }
+}
