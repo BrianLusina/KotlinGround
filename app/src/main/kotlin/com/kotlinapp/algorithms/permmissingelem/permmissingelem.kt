9179b0e75c588b8f3ef835fca649c06093671e62
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-06 16:25:40 +0300

    feat(algorithms): perm missing element

---
+++ app/src/main/kotlin/com/kotlinapp/algorithms/permmissingelem/permmissingelem.kt
@@ -0,0 +1,16 @@
+package com.kotlinapp.algorithms.permmissingelem
+
+/**
+ * Finds the missing element in a list of numbers
+ * @param numbers [Array<Int>]
+ * @return [Int] Missing element
+ */
+fun permMissingElement(numbers: Array<Int>): Int {
+    var missingElement = numbers.size + 1
+
+    numbers.forEachIndexed { index, it ->
+        missingElement = missingElement or it or (index + 1)
+    }
+
+    return missingElement
+}
