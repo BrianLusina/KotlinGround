45d35aaa1701abca98bb295cb24d738e86be9c61
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-03-11 14:37:32 +0300

    feat(binarytrees): in order traversal

--- app/src/main/kotlin/com/kotlinapp/datastructures/trees/Trees.kt
+++ app/src/main/kotlin/com/kotlinapp/datastructures/trees/Trees.kt
@@ -3,4 +3,8 @@ package com.kotlinapp.datastructures.trees
 abstract class Trees<TreeNode> {
 
     abstract fun addTreeNode(data: Any)
+
+    abstract fun inorderTraversalRecurse(root: TreeNode): Collection<Any>
+    abstract fun inorderTraversalIteratively(): Collection<Any>
+    abstract fun inorderMorrisTraversal(): Collection<Any>
 }
--- app/src/main/kotlin/com/kotlinapp/datastructures/trees/binarytrees/BinaryTree.kt
+++ app/src/main/kotlin/com/kotlinapp/datastructures/trees/binarytrees/BinaryTree.kt
@@ -7,4 +7,32 @@ class BinaryTree(private val root: BinaryTreeNode? = null) : Trees<BinaryTreeNod
     override fun addTreeNode(data: Any) {
         TODO("Not yet implemented")
     }
+
+    override fun inorderTraversalRecurse(root: BinaryTreeNode): Collection<Any> {
+        TODO("Not yet implemented")
+    }
+
+    override fun inorderTraversalIteratively(): Collection<Any> {
+        val stack = arrayListOf<BinaryTreeNode>()
+        val result = arrayListOf<Any>()
+        var current = root;
+
+        while (current != null || stack.isNotEmpty()) {
+            while (current != null) {
+                stack.add(current)
+                current = current.left
+            }
+
+            current = stack.removeAt(stack.size - 1)
+            result.add(current.data)
+            current = current.right
+        }
+
+        return result
+    }
+
+    override fun inorderMorrisTraversal(): Collection<Any> {
+        TODO("Not yet implemented")
+    }
+
 }
