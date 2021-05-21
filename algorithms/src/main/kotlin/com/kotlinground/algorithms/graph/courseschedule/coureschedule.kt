a5866b6c5612a6f3cfd0805a35b2b9f0c8010eb8
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-05-21 09:25:46 +0300

    feat(math): steps in primes

---
+++ algorithms/src/main/kotlin/com/kotlinground/algorithms/graph/courseschedule/coureschedule.kt
@@ -0,0 +1,50 @@
+package com.kotlinground.algorithms.graph.courseschedule
+
+const val WHITE = 1
+const val GRAY = 2
+const val BLACK = 3
+
+fun canFinish(numCourses: Int, prerequisites: Array<IntArray>): Boolean {
+    val adjacencyList = hashMapOf<Int, ArrayList<Int>>()
+    val color = hashMapOf<Int, Int>()
+
+    for (prerequisite in prerequisites) {
+        val destination = prerequisite[0]
+        val source = prerequisite[1]
+        adjacencyList[source]?.add(destination)
+    }
+
+    var isPossible = true
+
+    for (i in 0 until numCourses) {
+        color[i] = WHITE
+    }
+
+    fun dfs(node: Int) {
+        if (!isPossible) {
+            return
+        }
+
+        color[node] = GRAY
+
+        if (adjacencyList.containsKey(node)) {
+            for (neighbour in adjacencyList[node]!!) {
+                if (color[neighbour] == WHITE) {
+                    dfs(neighbour)
+                } else if (color[neighbour] == GRAY) {
+                    isPossible = false
+                }
+            }
+        }
+
+        color[node] = BLACK
+    }
+
+    for (i in 0 until numCourses) {
+        if (color[i] == WHITE) {
+            dfs(i)
+        }
+    }
+
+    return isPossible
+}
