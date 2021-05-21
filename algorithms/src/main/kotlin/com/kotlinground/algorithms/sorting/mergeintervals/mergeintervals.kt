3e031a73fe4fe31e355766239b640d1f5c681a04
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-05-21 22:53:20 +0300

    feat(algorithms): sort - merge intervals

---
+++ algorithms/src/main/kotlin/com/kotlinground/algorithms/sorting/mergeintervals/mergeintervals.kt
@@ -0,0 +1,18 @@
+package com.kotlinground.algorithms.sorting.mergeintervals
+
+import kotlin.math.max
+
+fun merge(intervals: Array<IntArray>): Array<IntArray> {
+    intervals.sortBy { it[0] }
+    val merged = arrayListOf<IntArray>()
+
+    for (interval in intervals) {
+        if (merged.size == 0 || merged.last()[1] < interval[0]) {
+            merged.add(interval)
+        } else {
+            merged.last()[1] = max(merged.last()[1], interval[1])
+        }
+    }
+
+    return merged.toTypedArray()
+}
