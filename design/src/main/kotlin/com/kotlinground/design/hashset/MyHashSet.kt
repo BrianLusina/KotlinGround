d4916cc84e0ffce92248348b3c06250064836d59
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2021-07-01 09:31:46 +0300

    feat(design): hashset

---
+++ design/src/main/kotlin/com/kotlinground/design/hashset/MyHashSet.kt
@@ -0,0 +1,24 @@
+package com.kotlinground.design.hashset
+
+class MyHashSet() {
+
+    /** Initialize your data structure here. */
+    var hash = HashSet<Int>()
+
+    fun add(key: Int) {
+        if (!contains(key)) {
+            hash.add(key)
+        }
+    }
+
+    fun remove(key: Int) {
+        if (contains(key)) {
+            hash.remove(key)
+        }
+    }
+
+    /** Returns true if this set contains the specified element */
+    fun contains(key: Int): Boolean {
+        return hash.contains(key)
+    }
+}
---
+++ design/src/test/kotlin/com/kotlinground/design/hashset/MyHashSetTest.kt
@@ -0,0 +1,17 @@
+package com.kotlinground.design.hashset
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+@Suppress("MagicNumber")
+class MyHashSetTest {
+    private val myHashSet = MyHashSet()
+
+    @Test
+    fun `Should add 1 to hashset`() {
+        val n = 1
+        myHashSet.add(n)
+
+        Assertions.assertEquals(hashSetOf(1), myHashSet.hash)
+    }
+}
