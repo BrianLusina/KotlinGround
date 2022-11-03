f58dca2aeb15bd786c3ff0f26ed93c562edc26f8
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-11-03 08:24:16 +0300

    feat(design): structural patterns - adapter

---
+++ design/src/main/kotlin/com/kotlinground/design/structuralpatterns/adapter/plugs.kt
@@ -0,0 +1,64 @@
+package com.kotlinground.design.structuralpatterns.adapter
+
+interface USPlug {
+    val hasPower: Int
+}
+
+interface EUPlug {
+    val hasPower: String
+}
+
+interface UsbMini {
+    val hasPower: Power
+}
+
+interface UsbTypeC {
+    val hasPower: Boolean
+}
+
+enum class Power {
+    TRUE, FALSE
+}
+
+fun cellPhone(chargeCable: UsbTypeC) {
+    if (chargeCable.hasPower) {
+        println("I've got the power")
+    } else {
+        println("No Power")
+    }
+}
+
+fun usPowerOutlet(): USPlug {
+    return object : USPlug {
+        override val hasPower: Int
+            get() = 1
+    }
+}
+
+fun charger(plug: EUPlug): UsbMini {
+    return object : UsbMini {
+        override val hasPower: Power
+            get() = Power.valueOf(plug.hasPower)
+    }
+}
+
+fun USPlug.toEUPlug(): EUPlug {
+    val hasPower = if (this.hasPower == 1) "TRUE" else "FALSE"
+    return object : EUPlug {
+        override val hasPower: String
+            get() = hasPower
+    }
+}
+
+fun UsbMini.toUsbTypeC(): UsbTypeC {
+    val hasPower = this.hasPower == Power.TRUE
+    return object : UsbTypeC {
+        override val hasPower: Boolean
+            get() = hasPower
+    }
+}
+
+
+fun main() {
+    cellPhone(charger(usPowerOutlet().toEUPlug()).toUsbTypeC())
+}
