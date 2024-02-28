57555ec23b592f905b341416028849dd13850d45
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-02-28 09:09:45 +0300

    feat(puzzles): gas stations

---
+++ puzzles/src/test/kotlin/com/kotlinground/puzzles/arrays/gasstations/CanCompleteCircuitTest.kt
@@ -0,0 +1,24 @@
+package com.kotlinground.puzzles.arrays.gasstations
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CanCompleteCircuitTest {
+    @Test
+    fun `should return 3 for gas = (1, 2, 3, 4, 5), cost = (3, 4, 5, 1, 2)`() {
+        val gas = intArrayOf(1, 2, 3, 4, 5)
+        val cost = intArrayOf(3, 4, 5, 1, 2)
+        val expected = 3
+        val actual = canCompleteCircuit(gas, cost)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should return -1 for gas = (2, 3, 4), cost = (3, 4, 3)`() {
+        val gas = intArrayOf(2, 3, 4)
+        val cost = intArrayOf(3, 4, 3)
+        val expected = -1
+        val actual = canCompleteCircuit(gas, cost)
+        assertEquals(expected, actual)
+    }
+}
