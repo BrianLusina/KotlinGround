05bedb645ef8b133f6d65d6463c217823170bc1e
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-04-10 19:51:09 +0300

    chore(coroutines): suspension sample

---
+++ concurrency/src/main/kotlin/com/kotlinground/concurrency/coroutines/suspension/sample.kt
@@ -0,0 +1,29 @@
+package com.kotlinground.concurrency.coroutines.suspension
+
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+val executor: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor {
+    Thread(it, "scheduler")
+        .apply {
+            isDaemon = true
+        }
+}
+
+suspend fun delay(timeMillis: Long): Unit {
+    suspendCoroutine {
+        executor.schedule({ it.resume(Unit) }, timeMillis, TimeUnit.MILLISECONDS)
+    }
+}
+
+
+suspend fun main() {
+    println("Before")
+
+    delay(1000)
+
+    println("After")
+}
